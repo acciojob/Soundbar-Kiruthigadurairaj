a91608fa3a48b5def8a6c9744e3e9fe85999e731
Kiruthigadurairaj <146357922+Kiruthigadurairaj@users.noreply.github.com>
2025-01-25 18:50:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,19 @@
-const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+function soundPlay(audioPlayer){ 
+let audioPlayer = document.getElementsByClassName("btn");
+audioPlayer.src = "D:\New folder\sound\short-crowd-cheer-6713.mp3"+ soundbar;
+audioPlayer.play();
+}
+btns.addEventListener("click", soundPlay);
+function soundPlay(audioPlayer){ 
+    let audioPlayer = document.getElementsByClassName("btn");
+    audioPlayer.src = "D:\New folder\sound\boo-6377.mp3" + soundbar;
+    audioPlayer.play();
+    }
+    btns.addEventListener("click", soundPlay);
 
-        const buttonsContainer = document.getElementById("buttons");
 
-        // Function to stop all sounds
-        const stopAllSounds = () => {
-            const audioElements = document.querySelectorAll("audio");
-            audioElements.forEach(audio => {
-                audio.pause();
-                audio.currentTime = 0;
-            });
-        };
-
-        // Dynamically create buttons for each sound
-        sounds.forEach(sound => {
-            const button = document.createElement("button");
-            button.className = "btn";
-            button.textContent = sound;
-
-            button.addEventListener("click", () => {
-                stopAllSounds();
-                const audio = new Audio(`sounds/${sound}.mp3`);
-                audio.play();
-            });
-
-            buttonsContainer.appendChild(button);
-        });
-
-        // Create the stop button
-        const stopButton = document.createElement("button");
-        stopButton.className = "btn stop";
-        stopButton.textContent = "stop";
-
-        stopButton.addEventListener("click", stopAllSounds);
-
-        buttonsContainer.appendChild(stopButton);
-   
+function soundStop(){
+    let audioPlayer = getElementsByClassName("stop");
+    audioPlayer.pause();
+    audioPlayer.currentTime = 0;
+}
